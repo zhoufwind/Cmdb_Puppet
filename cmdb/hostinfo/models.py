fce694a4056403c2a5c62289aa10c652150753eb
davidbj <david_bj@126.com>
2013-08-02 16:25:13 +0800

    cmdb model.py update

--- cmdb/hostinfo/models.py
+++ cmdb/hostinfo/models.py
@@ -1,4 +1,7 @@
 from django.db import models
+from django.db.models.signals import pre_save
+from django.dispatch import receiver
+import paramiko
 
 # Create your models here.
 
@@ -25,7 +28,7 @@ class Host(models.Model):
     identity = models.CharField(max_length=50,null=True)
  
     def __unicode__(self):
-        return self.hostname
+        return '%s,%s' % (self.hostname,self.ipaddr)
     
 #class IPaddr(models.Model):
 #     ipaddr = models.IPAddressField()
@@ -34,3 +37,21 @@ class Host(models.Model):
 class HostGroup(models.Model):
     name = models.CharField(max_length=30)
     members = models.ManyToManyField(Host)
+
+@receiver(pre_save,sender=Host)
+def mod_handler(sender,**kwargs):
+    data = str(kwargs['instance'])
+    host, ip = data.split(',')
+    user = 'root'
+    passwd = '123456'
+    port = 22
+    connectExec(ip,port,user,passwd,host)
+
+def connectExec(ip,port,user,passwd,host):
+    ssh = paramiko.SSHClient()
+    paramiko.util.log_to_file('/var/tmp/connectExec.log')
+    ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy())
+    ssh.connect(hostname=ip,port=port,username=user,password=passwd)
+    ssh.exec_command('echo %s %s >> /etc/hosts' % (ip,host))
+    ssh.exec_command("sed -i 's/^HOSTNAME.*/HOSTNAME=%s/' /etc/sysconfig/network" % host)
+    ssh.close()
